a3d871398a6c32fa85db3c8159164bd433461395
Unai Lecue <unailecuea@gmail.com>
2021-12-29 23:39:20 -0600

    :art:Reference between Risk and Reward

--- src/components/PlanningInput.js
+++ src/components/PlanningInput.js
@@ -1,24 +1,26 @@
 
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Form, Row, Col, InputGroup } from 'react-bootstrap';
 import * as utils from "../utils/utils";
 import { Trans } from 'react-i18next';
+import { useState } from 'react';
 const TITLE = <Trans>Plan</Trans>
 const REFERENCE_ENTRY_NAME = <Trans>Reference Entry</Trans>
 const REFERENCE_SHARES_NAME = <Trans>Reference Shares</Trans>
+const RISK_REWARD_VALIDATION = <Trans>The reference value should be between reward and risk</Trans>
 
 export default function PlanningInput({ planningInput }) {
     const refEntry = useRef()
     const referenceShare = planningInput.referenceSharesObj.referenceShare;
+    // const [invalidValue, setInvalidValue] = useState(false)
+    const invalidValue = planningInput.invalidValueReferenceEntry;
 
     function handleChangeRefenceEntry(e) {
         planningInput.referenceEntryObj.setState(parseFloat(refEntry.current.value));
     }
     return (
         <div className="personal-box-shadow">
-            <h5>
-                {TITLE}
-            </h5>
+            <h5>{TITLE}</h5>
             <Row>
                 <Col sm="6">
                     <Form.Group as={Row} className="mb-3 align-items-center">
@@ -27,8 +29,9 @@ export default function PlanningInput({ planningInput }) {
                         </Form.Label>
                         <Col xs="12">
                             <InputGroup>
-                                <Form.Control type="number" ref={refEntry} onChange={handleChangeRefenceEntry} />
+                                <Form.Control type="number" ref={refEntry} onChange={handleChangeRefenceEntry} isInvalid={invalidValue} />
                                 <InputGroup.Text> $/sh</InputGroup.Text>
+                                <Form.Control.Feedback type="invalid">{RISK_REWARD_VALIDATION}</Form.Control.Feedback>
                             </InputGroup>
                         </Col>
                     </Form.Group>
@@ -40,7 +43,6 @@ export default function PlanningInput({ planningInput }) {
                         </Form.Label>
                         <Col xs="12">
                             <InputGroup>
-
                                 <Form.Control type="number" readOnly value={referenceShare} />
                                 <InputGroup.Text> sh</InputGroup.Text>
                             </InputGroup>
