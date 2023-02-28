610fd9a78e8f37b7ce1a8a9985844121880ee38b
Unai Lecue <unailecuea@gmail.com>
2023-02-28 10:09:23 -0600

    :lipstick:Updating header style

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,12 @@ const Header = () => {
     return (
         <div id="main-header">
             <h1>
-                Day Trader Latino
+                    <a className="header-inner section-inner"  href='https://daytraderlatino.com/'>
+                        <a href="https://daytraderlatino.com/" className="custom-logo-link" rel="home" aria-current="page">
+                            <img width="736" height="731" src="https://daytraderlatino.files.wordpress.com/2020/04/cropped-img_9500.png" className="custom-logo" alt="Day Trader Latino" decoding="async" srcset="https://daytraderlatino.files.wordpress.com/2020/04/cropped-img_9500.png?w=736&amp;h=731 736w, https://daytraderlatino.files.wordpress.com/2020/04/cropped-img_9500.png?w=1472&amp;h=1462 1472w, https://daytraderlatino.files.wordpress.com/2020/04/cropped-img_9500.png?w=150&amp;h=150 150w, https://daytraderlatino.files.wordpress.com/2020/04/cropped-img_9500.png?w=300&amp;h=298 300w, https://daytraderlatino.files.wordpress.com/2020/04/cropped-img_9500.png?w=768&amp;h=763 768w, https://daytraderlatino.files.wordpress.com/2020/04/cropped-img_9500.png?w=1024&amp;h=1017 1024w" sizes="(max-width: 736px) 100vw, 736px" data-attachment-id="83" data-permalink="https://daytraderlatino.com/cropped-img_9500-png/" data-orig-file="https://daytraderlatino.files.wordpress.com/2020/04/cropped-img_9500.png" data-orig-size="1546,1536" data-comments-opened="1" data-image-meta="{&quot;aperture&quot;:&quot;0&quot;,&quot;credit&quot;:&quot;&quot;,&quot;camera&quot;:&quot;&quot;,&quot;caption&quot;:&quot;&quot;,&quot;created_timestamp&quot;:&quot;0&quot;,&quot;copyright&quot;:&quot;&quot;,&quot;focal_length&quot;:&quot;0&quot;,&quot;iso&quot;:&quot;0&quot;,&quot;shutter_speed&quot;:&quot;0&quot;,&quot;title&quot;:&quot;&quot;,&quot;orientation&quot;:&quot;0&quot;}" data-image-title="cropped-img_9500.png" data-image-description="<p>https://daytraderlatino.files.wordpress.com/2020/04/cropped-img_9500.png</p>" data-image-caption="" data-medium-file="https://daytraderlatino.files.wordpress.com/2020/04/cropped-img_9500.png?w=300" data-large-file="https://daytraderlatino.files.wordpress.com/2020/04/cropped-img_9500.png?w=736"></img></a>
+                        <p className="site-title"><a href="https://daytraderlatino.com/" rel="home">Day Trader Latino</a></p>
+                        <p className="site-description" >Lo que necesitas saber sobre Day Trading en tu idioma</p>
+                </a>
             </h1>
         </div>
     );
