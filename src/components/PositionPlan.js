a45c24a25f452c89f1c10f8a1dc293c0d80fc4ec
Unai Lecue <unailecuea@gmail.com>
2025-02-10 22:59:46 -0600

    removing round error

--- src/components/PositionPlan.js
+++ src/components/PositionPlan.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap';
 import PlanningInput from './PlanningInput';
 import PlanningResults from './PlanningResults';
